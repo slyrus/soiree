6bccfd3d7eb2b26459b10f010760d606d2e338dc
Cyrus Harmon <ch-lisp@bobobeach.com>
2012-03-02 14:24:32 -0800

    add *vcard-rng-pathname* and *vcard-rng-schema*

--- package.lisp
+++ package.lisp
@@ -2,4 +2,13 @@
 (cl:defpackage :cl-vcard
   (:use :common-lisp :parser-combinators)
   (:nicknames :vcard)
-  (:export :parse-vcard))
+  (:export #:parse-vcard
+           #:*vcard-rng-pathname*
+           #:*vcard-rng-schema*))
+
+(in-package :cl-vcard)
+
+(defvar *vcard-rng-pathname*
+  (merge-pathnames #p"vcard-4_0.rng" cl-vcard-config:*base-directory*))
+
+(defvar *vcard-rng-schema* (cxml-rng:parse-compact *vcard-rng-pathname*))
